cd1a49959eeabe581bdcfd0ce2c86920ec4e11a2
AlessandroPerez27 <avperez@fi.uba.ar>
2026-05-01 21:52:19 -0300

    Se corrigen tests de integracion

--- test/condor_del_sur_test.exs
+++ test/condor_del_sur_test.exs
@@ -1,26 +1,43 @@
 defmodule CondorDelSurTest do
   use ExUnit.Case
-  alias CondorDelSur.{Vuelo, Asiento, ServidorVuelo}
+  alias CondorDelSur.{Vuelo, Asiento, ServidorVuelo, RegistradorAuditoria}
 
-  # Levanta un servidor fresco antes de cada test con timeout corto para expiracion
-  setup do
+  # Helpers para no repetir la inicializacion
+  defp iniciar_sistema(timeout_expiracion) do
+    # Mata cualquier proceso registrado previo antes de levantar uno nuevo
+    if pid = Process.whereis(:registrador_auditoria) do
+      Process.exit(pid, :kill)
+      Process.sleep(20)
+    end
+
+    if pid = Process.whereis(:servidor_vuelo) do
+      Process.exit(pid, :kill)
+      Process.sleep(20)
+    end
+
     vuelo = Vuelo.nuevo("AR1234", "Buenos Aires", "Bariloche", "2026-07-15")
     asientos = [Asiento.nuevo("1A", 1, "A"), Asiento.nuevo("1B", 1, "B")]
-    pid = ServidorVuelo.iniciar(vuelo, asientos, 1_000)
+    pid_logger = RegistradorAuditoria.iniciar()
+    Process.sleep(50)
+    pid_servidor = ServidorVuelo.iniciar(vuelo, asientos, timeout_expiracion)
+    {pid_servidor, pid_logger}
+  end
 
-    # Limpia el proceso registrado al terminar cada test
-    on_exit(fn ->
-      if Process.alive?(pid), do: Process.exit(pid, :kill)
-      # Espera a que se limpie el registro antes del proximo test
-      Process.sleep(50)
-    end)
+  defp detener_sistema(pid_servidor, pid_logger) do
+    if Process.alive?(pid_servidor), do: Process.exit(pid_servidor, :kill)
+    if Process.alive?(pid_logger), do: Process.exit(pid_logger, :kill)
+    Process.sleep(50)
+  end
 
-    %{pid: pid}
+  # Setup con 5 segundos de expiracion, suficiente para que el pago (500ms) llegue antes
+  setup do
+    {pid_servidor, pid_logger} = iniciar_sistema(5_000)
+    on_exit(fn -> detener_sistema(pid_servidor, pid_logger) end)
+    %{pid_servidor: pid_servidor, pid_logger: pid_logger}
   end
 
   test "reservar asiento disponible retorna ok con id de reserva" do
     send(:servidor_vuelo, {:reservar, "1A", "P1", self()})
-
     assert_receive {:ok, id_reserva}, 1_000
     assert String.starts_with?(id_reserva, "R")
   end
@@ -64,15 +81,17 @@ defmodule CondorDelSurTest do
     assert_receive {:error, :no_pendiente}, 1_000
   end
 
-  # Usa timeout de 1 segundo configurado en setup, espera 2s para que expire
+  # Este test levanta su propio sistema con timeout corto, independiente del setup
   test "reserva expirada libera el asiento" do
-    send(:servidor_vuelo, {:reservar, "1A", "P1", self()})
+    {pid_servidor, pid_logger} = iniciar_sistema(1_000)
+
+    send(pid_servidor, {:reservar, "1A", "P1", self()})
     assert_receive {:ok, _id_reserva}, 1_000
 
-    # Espera que expire (timeout configurado en 1s en setup)
-    Process.sleep(2_000)
+    # Espera que expire (timeout 1s + margen)
+    Process.sleep(1_500)
 
-    send(:servidor_vuelo, {:obtener_estado, self()})
+    send(pid_servidor, {:obtener_estado, self()})
     assert_receive {:estado, estado}, 1_000
 
     reserva = estado.reservas |> Map.values() |> List.first()
@@ -80,5 +99,7 @@ defmodule CondorDelSurTest do
 
     assert reserva.estado == :expired
     assert asiento.estado == :available
+
+    detener_sistema(pid_servidor, pid_logger)
   end
 end
